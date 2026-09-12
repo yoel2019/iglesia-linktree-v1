86b845d2cb8f2de1ce6194c3733e72d26799c0cc
Yoel Laya <52616876+yoel2019@users.noreply.github.com>
2026-09-12 11:35:29 -0400

    Add Supabase client

---
+++ lib/supabase.ts
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js';
+
+export function supabaseBrowser() {
+  return createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
+  );
+}
