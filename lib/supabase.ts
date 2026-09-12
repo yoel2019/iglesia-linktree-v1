2c0c96fb83c3ac505479e41ef5e238ad9ef48d70
Yoel Laya <52616876+yoel2019@users.noreply.github.com>
2026-09-12 12:52:50 -0400

    Use browser-session auth storage

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,6 +3,14 @@ import { createClient } from '@supabase/supabase-js';
 export function supabaseBrowser() {
   return createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+    {
+      auth: {
+        persistSession: true,
+        storage: typeof window !== 'undefined' ? window.sessionStorage : undefined,
+        autoRefreshToken: true,
+        detectSessionInUrl: true,
+      },
+    }
   );
 }
